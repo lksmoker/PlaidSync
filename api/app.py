7b8bf6df360aca36790ca2c79383ff01bb8edbbc
lksmoker <Luke.smoker@gmail.com>
2025-03-15 12:33:52 +0000

    Set port to 1000

--- api/app.py
+++ api/app.py
@@ -39,4 +39,4 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
